617214ff2306226572d7217b6ad4794f791017bc
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 19:13:13 +0100

    Landscaper: QUnit2 upgrade

--- can-route-mock-test.js
+++ can-route-mock-test.js
@@ -3,12 +3,12 @@ var RouteMock = require('./can-route-mock');
 
 QUnit.module('can-route-mock');
 
-QUnit.test('basics', function(){
+QUnit.test('basics', function(assert) {
     var mock = new RouteMock();
 
-    QUnit.equal(mock.value, "", "read as empty");
+    assert.equal(mock.value, "", "read as empty");
 
     mock.value = "#foo/bar";
 
-    QUnit.equal(mock.value, "foo/bar");
+    assert.equal(mock.value, "foo/bar");
 });
